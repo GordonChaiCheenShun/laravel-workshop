092bb533393f488bbc0aa14bb424ccf0f428d758
gordonchaicheenshun <gordoninfosec@gmail.com>
2019-03-30 16:10:01 +0800

    first commit

---
+++ app/Http/Controllers/GordonController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GordonController extends Controller
+{
+    public function index()
+    {
+        return view('gordon');
+    }
+
+   
+}
